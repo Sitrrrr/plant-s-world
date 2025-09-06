95dcad02492a8dc57a2366949474c62e5f70eea1
Sitrrrr <petrushenas@gmail.com>
2025-09-06 16:52:53 +0300

    commit

--- aa.js
+++ aa.js
@@ -2,10 +2,18 @@ const container = document.getElementById('cards-container');
 
     // Дані для кожної рослини
     const plants = [
-      { name: "Фікус Бенджаміна", description: "Декоративна рослина з блискучим листям", link: "ficus.html", id: "ficus" },
-      { name: "Драцена", description: "Екзотична рослина з довгим листям", link: "dracaena.html", id: "dracaena" },
-      { name: "Сукулент Ехеверія", description: "Маленький сукулент з розеткою", link: "echeveria.html", id: "echeveria" },
-      // Додай ще 17 рослин тут
+      { name: "Фікус Бенджаміна", description: "Елегантне деревце з густою кроною та маленькими блискучими листочками.", link: "ficusbenjemin.html", id: "ficus" },
+      { name: "Фікус Лірата", description: "великі, глянцеві листки у формі скрипки роблять його «зіркою інтер’єру»", link: "dracaena.html", id: "dracaena" },
+      { name: "Монстера делікатесна", description: "Великі розрізані листки, справжня класика", link: "echeveria.html", id: "echeveria" },
+      { name: "Бегонія королівська", description: "Декоративне листя з фантастичними візерунками", link: "ficus.html", id: "ficus" },
+      {name: "Цикламен перський", description: "Яскраві квіти, довготривале цвітіння", link: "ficus.html", id: "ficus" },
+    { name: "Фікус Еластика", description: "має товсті, темно-зелені або строкаті листки. Дуже витривалий і простий у догляді", link: "ficus.html", id: "ficus" },
+     { name: "Монстера варієгатна", description: "Біла чи кремова варіація на листках, рідкість", link: "ficus.html", id: "ficus" },
+    { name: "Бегонія вічноквітуча", description: "Квітує цілий рік", link: "ficus.html", id: "ficus" },
+    { name: "Міні-цикламени", description: "Компактні та милі, ідеально для подарунків", link: "ficus.html", id: "ficus" },
+    { name: "Товстянка Хобіт", description: "Оригінальні трубчасті листки", link: "ficus.html", id: "ficus" },
+     { name: "Хавортія купчаста", description: "Листки прозорі на кінчиках, виглядає дуже ефектно", link: "ficus.html", id: "ficus" },
+    { name: "Хавортія маргарітіфера", description: "Великі білі крапки на темно-зелених листках", link: "ficus.html", id: "ficus" },
     ];
 
     plants.forEach(plant => {
@@ -40,3 +48,4 @@ const container = document.getElementById('cards-container');
       menuOpen = !menuOpen;
       sideMenu.classList.toggle('show', menuOpen);
     });
+
